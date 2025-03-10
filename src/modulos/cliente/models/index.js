f0a4c577bc4a6756ba3e1e3cb663b85794894e18
BeatrizCollier <beatrizmcollier@hotmail.com>
2025-03-10 17:02:46 -0300

    fix: modifiquei o index.js do models.

--- src/modulos/cliente/models/index.js
+++ src/modulos/cliente/models/index.js
@@ -2,7 +2,7 @@ const pool = require('../../../config/database');
 
 function ListarProdutos(){
     try {
-        const query = `select * from produto`;
+        const query = `select * from orcamento`;
         const resultado = pool.query(query)
         if(!resultado){
             
